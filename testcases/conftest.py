7bf55ac4a1ffaf4e3b824df0b841a99ffe48ce3f
Miftah <miftah.ramadhan@noble-software.com>
2023-09-26 17:53:13 +0700

    fixing error options=options

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,6 +1,9 @@
 import time
 
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import pytest
 
 @pytest.fixture()
@@ -10,11 +13,11 @@ def setup(request):
     options.add_experimental_option("detach", True)
     options.add_argument("--disable-notifications")  # for chrome only
     options.add_argument('--headless')
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     #driver.maximize_window()
     driver.get('https://portalmurid.com')
     request.cls.driver = driver
     yield
-    driver.quit() # test commit 2
+    driver.quit()
 
 
--- testcases/test_login.py
+++ testcases/test_login.py
@@ -4,66 +4,75 @@ from pages.loginpage import LoginPage
 from pages.dashboard import DashboardPage
 
 USERID_ADMIN_VALID = "admin_uat"
-USERID_ADMIN_INVALID = "ADMIN_UAAAATTTXXX"
 VALID_PASSWORD_ADMIN = "password123*"
-INVALID_PASSWORD_ADMIN = "xccxfef111"
-BLANK_INPUT = ""
+USERID_GURU_MATPELK13_VALID = "sub_teacher_k13"
+VALID_PASSWORD_GURU_MATPELK13 = "password123*"
+USERID_GURU_WALIK13_VALID = "sub_teacher_km"
+VALID_PASSWORD_GURU_WALIK13 = "password123*"
 
+USERID_GURU_MATPELKM_VALID = "sub_teacher_k13"
+VALID_PASSWORD_GURU_MATPELKM = "password123*"
+USERID_GURU_WALIKM_VALID = "sub_teacher_km"
+VALID_PASSWORD_GURU_WALIKM = "password123*"
+
+USERID_STUDENT_VALID = ""
+VALID_PASSWORD_STUDENT = "password123*"
+USERID_INVALID = "USERID_INVALID@*#"
+INVALID_PASSWORD = "INVALID_PASS123*#"
+BLANK_INPUT = ""
 
 @pytest.mark.usefixtures("setup")
 class TestLogin:
     """-------------------------------------SCENARIO LOGIN BY ADMIN--------------------------------------------"""
-    def test_input_invalid_Password(self):
-        tl = LoginPage(self.driver)
-        tl.inputUserId(USERID_ADMIN_VALID)
-        tl.inputPassword(INVALID_PASSWORD_ADMIN)
-        tl.clickLoginButton()
-        tl.verify_toast_failed_login("Nama Pengguna atau Kata Sandi Salah")
+    def test_login_invalid_Password(self):
+        admin = LoginPage(self.driver)
+        admin.inputUserId(USERID_ADMIN_VALID)
+        admin.inputPassword(INVALID_PASSWORD)
+        admin.clickLoginButton()
+        admin.verify_toast_failed_login("Nama Pengguna atau Kata Sandi Salah")
 
-    def test_input_invalid_UserID(self):
-        tl = LoginPage(self.driver)
-        tl.inputUserId(USERID_ADMIN_INVALID)
-        tl.inputPassword(VALID_PASSWORD_ADMIN)
-        tl.clickLoginButton()
-        tl.verify_toast_failed_login("Nama Pengguna atau Kata Sandi Salah")
+    def test_login_invalid_UserID(self):
+        admin = LoginPage(self.driver)
+        admin.inputUserId(USERID_INVALID)
+        admin.inputPassword(VALID_PASSWORD_ADMIN)
+        admin.clickLoginButton()
+        admin.verify_toast_failed_login("Nama Pengguna atau Kata Sandi Salah")
 
-    def test_input_invalid_UserID_and_Password(self):
-        tl = LoginPage(self.driver)
-        tl.inputUserId(USERID_ADMIN_INVALID)
-        tl.inputPassword(INVALID_PASSWORD_ADMIN)
-        tl.clickLoginButton()
-        tl.verify_toast_failed_login("Nama Pengguna atau Kata Sandi Salah")
+    def test_login_invalid_UserID_and_Password(self):
+        admin = LoginPage(self.driver)
+        admin.inputUserId(USERID_INVALID)
+        admin.inputPassword(INVALID_PASSWORD)
+        admin.clickLoginButton()
+        admin.verify_toast_failed_login("Nama Pengguna atau Kata Sandi Salah")
 
-    def test_input_blank_UserID(self):
-        tl = LoginPage(self.driver)
-        tl.inputUserId(BLANK_INPUT)
-        tl.inputPassword(VALID_PASSWORD_ADMIN)
-        tl.clickLoginButton()
-        tl.verify_toast_failed_login('Nama Pengguna kosong')
+    def test_login_blank_UserID(self):
+        admin = LoginPage(self.driver)
+        admin.inputUserId(BLANK_INPUT)
+        admin.inputPassword(VALID_PASSWORD_ADMIN)
+        admin.clickLoginButton()
+        admin.verify_toast_failed_login('Nama Pengguna kosong')
 
-    def test_input_blank_Password(self):
-        tl = LoginPage(self.driver)
-        tl.inputUserId(USERID_ADMIN_VALID)
-        tl.inputPassword(BLANK_INPUT)
-        tl.clickLoginButton()
-        tl.verify_toast_failed_login('Kata Sandi kosong')
+    def test_login_blank_Password(self):
+        admin = LoginPage(self.driver)
+        admin.inputUserId(USERID_ADMIN_VALID)
+        admin.inputPassword(BLANK_INPUT)
+        admin.clickLoginButton()
+        admin.verify_toast_failed_login('Kata Sandi kosong')
 
+    def test_login_blankUserID_and_blankPassword(self):
+        admin = LoginPage(self.driver)
+        admin.inputUserId(BLANK_INPUT)
+        admin.inputPassword(BLANK_INPUT)
+        admin.clickLoginButton()
+        admin.verify_toast_failed_login('Nama Pengguna & Kata Sandi kosong')
 
-    def test_input_blankUserID_and_BlankPassword(self):
-        tl = LoginPage(self.driver)
-        tl.inputUserId(BLANK_INPUT)
-        tl.inputPassword(BLANK_INPUT)
-        tl.clickLoginButton()
-        tl.verify_toast_failed_login('Nama Pengguna & Kata Sandi kosong')
-
-    def test_input_valid_userid_Admin_and_valid_password(self):
-        tl = LoginPage(self.driver)
-        tl.inputUserId(USERID_ADMIN_VALID)
-        tl.inputPassword(VALID_PASSWORD_ADMIN)
-        tl.clickLoginButton()
-        dp = DashboardPage(self.driver)
-        dp.verify_text_welcome_page('Selamat datang')
-        # ----
+    def test_login_valid_userid_and_valid_password(self):
+        admin = LoginPage(self.driver)
+        admin.inputUserId(USERID_ADMIN_VALID)
+        admin.inputPassword(VALID_PASSWORD_ADMIN)
+        admin.clickLoginButton()
+        dashboard = DashboardPage(self.driver)
+        dashboard.verify_text_welcome_page('Selamat datang')
 
     """-------------------------------------SCENARIO LOGIN BY TEACHER--------------------------------------------"""
 
